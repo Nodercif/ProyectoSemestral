71d72a0dc75f0c0942f0f4fc43b649592eeb2941
Nodercif <gamunoz2022@udec.cl>
2023-11-28 13:27:47 -0300

    Clase mob

--- Visual/Mob.java
+++ Visual/Mob.java
@@ -1,13 +1,25 @@
 package Visual;
 
+import java.util.ArrayList;
+class Punto{
+    int x;
+    int y;
+}
 public abstract class Mob {
     private int velocidad;
     private int tiempo;
+    private int posicion;
+    private ArrayList<Punto> camino;
     public Mob(){
+        camino = new ArrayList<Punto>();
+        String archivoDeCaminos = "recursos/caminos.txt";
     }
-    public void movimientoEntidad() {
-        //Trabaja con las variables velocidad y tiempo.
+    public void tick() {
+        //caminar hacia camino.firt
+        //TODO
+        /*posicicion += (camino.get(0)-posicion)/algo;
+        if(posicion - camino.get(0)<3)camino.remove(0);
+        //Trabaja con las variables velocidad y tiempo.*/
+        
     }
-
-
 }

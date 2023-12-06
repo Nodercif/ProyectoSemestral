e8ad88ebe515485a0bb5f73f37619147bfbec423
Felicia <faguin2022@udec.cl>
2023-12-06 13:01:03 -0300

    implementar de movimiento de la clase Mob

--- Visual/Mob.java
+++ Visual/Mob.java
@@ -1,25 +1,40 @@
 package Visual;
 
 import java.util.ArrayList;
+import java.lang.Math;
 class Punto{
-    int x;
-    int y;
+    float x;
+    float y;
+    public Punto(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
 }
 public abstract class Mob {
-    private int velocidad;
-    private int tiempo;
-    private int posicion;
+    private float rapidez;
+    private Punto posicion;
     private ArrayList<Punto> camino;
     public Mob(){
         camino = new ArrayList<Punto>();
         String archivoDeCaminos = "recursos/caminos.txt";
     }
     public void tick() {
-        //caminar hacia camino.firt
-        //TODO
-        /*posicicion += (camino.get(0)-posicion)/algo;
-        if(posicion - camino.get(0)<3)camino.remove(0);
-        //Trabaja con las variables velocidad y tiempo.*/
-        
+        Punto siguiente = camino.get(0);
+        float dx = siguiente.x - posicion.x;
+        float dy = siguiente.y - posicion.x;
+        if( (dx*dx + dy*dy)<25 )camino.remove(0);
+        float norma = (float)Math.sqrt(dx*dx+dy*dy);
+        posicion.x += rapidez * dx/norma;
+        posicion.y += rapidez * dy/norma;
+    }
+    public void addDestino(int x, int y){
+        camino.add(new Punto(x,y));
+    }
+
+    public int getPosX(){return (int)posicion.x;}
+    public int getPosY(){return (int)posicion.y;}
+    public void setRapidez(float rapidez){
+        this.rapidez = rapidez;
+        if(this.rapidez >= 5)this.rapidez = 4.99f;
     }
 }

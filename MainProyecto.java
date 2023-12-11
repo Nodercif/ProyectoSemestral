7e42179a2a9c9a8708a55145485408e2a9989c3b
Nodercif <gamunoz2022@udec.cl>
2023-12-10 23:48:26 -0300

    Main

--- MainProyecto.java
+++ MainProyecto.java
@@ -1,5 +1,6 @@
 import Visual.Ventana;
-
+/**@author Felicia Pino
+ * @author Gabriela Muñoz */
 public class MainProyecto {
     public static void main(String[] args){
         Ventana.getInstance();
